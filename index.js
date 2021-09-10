f8b9af7ca841ceb870c25a041342fc59a33042f5
MikaelOmarTarhuni <84583156+MikaelOmarTarhuni@users.noreply.github.com>
2021-09-10 03:56:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ const Directors = Models.Director;
 
 app.use(morgan("common"));
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true });
 
-//mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Middleware
 app.use(bodyParser.json());
