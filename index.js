d5881aedbf1d1c6601a19779702896212cec23b2
MikaelOmarTarhuni <84583156+MikaelOmarTarhuni@users.noreply.github.com>
2021-09-13 16:28:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 // const cors = require('cors');
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://myflixdb-mikael.herokuapp.com/', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'https://myflixdb-mikael.herokuapp.com/', 'http://localhost:1234'];
 app.use(cors({
 	origin: (origin, callback) => {
 	  if(!origin) return callback(null, true);
